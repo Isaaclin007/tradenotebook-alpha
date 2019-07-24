db6d39eee25b764889cf0a2a025cda87923ce9b5
Jerodsun <Jerod.sun@live.com>
2019-07-24 08:36:04 -0700

    working backend calculation implementation

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from zi.views import PyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', PyView.as_view())
 ]
--- zi/views.py
+++ zi/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import serializers, views
+from rest_framework.response import Response
+
+from .pyziabm_run import main
 
 # Create your views here.
+
+# class InputSerializer(serializers.Serializer):
+#     model_input = serializers.CharField()
+# current implementation does not require input 
+
+class PyView(views.APIView):
+    
+    def get(self, request):
+        # serializer = InputSerializer()
+        df = main()
+        return Response({'message':'success', 'data': df})
