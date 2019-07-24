cb4798073176f30df864d9d3071a35570a01fbc2
Jerodsun <Jerod.sun@live.com>
2019-07-24 11:59:44 -0700

    ModelSerializer, ModelViewSet implementation

--- zi/models.py
+++ zi/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Result(models.Model):
+    created = models.DateTimeField(auto_now_add = True)
+    model_input = models.CharField(max_length = 100)
+    delivered = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created']
--- zi/views.py
+++ zi/views.py
@@ -1,17 +1,50 @@
-from rest_framework import serializers, views
+from rest_framework import views, viewsets
 from rest_framework.response import Response
 
 from .pyziabm_run import main
 
+from .serializers import InputSerializer
+from .models import Result
 # Create your views here.
 
-# class InputSerializer(serializers.Serializer):
-#     model_input = serializers.CharField()
-# current implementation does not require input 
 
-class PyView(views.APIView):
+# class PyView(views.APIView):
+#     queryset = Result.objects.all()
+#     serializer_class = InputSerializer
+#     def get(self, request):
+#         return Response({'message':'please POST arbitrary data'})
     
+#     def post(self, request):
+#         serializer = InputSerializer(data = request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             df = main()
+#             return Response({'message':'success', 'params':serializer.validated_data, 'data': df})
+#         return Response({'message': 'error!'})
+
+
+class PyView(viewsets.ModelViewSet):
+    serializer_class = InputSerializer
+    queryset = Result.objects.all()
+    http_method_names = ['get', 'post']
+
+    def list(self, request):
+        return Response({'message':'please POST arbitrary data'})
+        
+    def retrieve(self, request, pk):
+        return Response({'message':'please POST arbitrary data'})
+    
+    def create(self, request):
+        serializer = InputSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            df = main()
+            return Response({'message':'success', 'params':serializer.validated_data, 'data': df})
+        return Response({'message': 'error!'})
+      
+
+class RandomView(views.APIView):
     def get(self, request):
-        # serializer = InputSerializer()
-        df = main()
-        return Response({'message':'success', 'data': df})
+        return Response({'message': 'hi!'})
