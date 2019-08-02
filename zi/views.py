00b934f56f27d48de5b76f86d6dad1846d38056c
Jerodsun <Jerod.sun@live.com>
2019-08-01 22:51:29 -0700

    view code further cleanup

--- zi/views.py
+++ zi/views.py
@@ -8,28 +8,24 @@ from .models import Result
 
 # Create your views here.
 
-# OLD class PyView(views.APIView)
-
 class PyView(viewsets.ModelViewSet):
     serializer_class = InputSerializer
     queryset = Result.objects.all()
     http_method_names = ['get', 'post']
 
-    def list(self, request):
-        return Response({'message':'please POST arbitrary data'})
-        
-    def retrieve(self, request, pk):
-        return Response({'message':'please POST arbitrary data'})
-    
     def create(self, request):
         serializer = InputSerializer(data = request.data)
-
         if serializer.is_valid():
             serializer.save()
             df = main()
             return Response({'message':'success', 'params':serializer.validated_data, 'data': df})
         return Response({'message': 'error!'})
-      
+
+    def list(self, request):
+        return Response({'message':'please POST arbitrary data'})
+
+    def retrieve(self, request, pk):
+        return Response({'message':'please POST arbitrary data'})
 
 class RandomView(views.APIView):
     def get(self, request):
