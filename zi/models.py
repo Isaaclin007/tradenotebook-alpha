7b6aece6a57ffa5b3da1118d4aefc657108fb0ae
Jerodsun <Jerod.sun@live.com>
2019-08-01 23:08:51 -0700

    get the user ip upon each request

--- zi/models.py
+++ zi/models.py
@@ -6,6 +6,6 @@ class Result(models.Model):
     created = models.DateTimeField(auto_now_add = True)
     model_input = models.CharField(max_length = 100)
     delivered = models.BooleanField(default=False)
-
+    ip_address = models.CharField(max_length = 15)
     class Meta:
         ordering = ['created']
