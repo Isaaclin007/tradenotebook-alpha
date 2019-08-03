a80b48bb6e10fcda30270abd7544e7fd2d197cd3
Jerodsun <Jerod.sun@live.com>
2019-08-02 21:14:24 -0700

    implement heroku deploy

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,8 @@ STATIC_URL = '/static/'
 # https://pypi.org/project/django-cors-headers/
 
 CORS_ORIGIN_WHITELIST = []
+
+
+# Heroku deploy
+
+django_heroku.settings(locals())
